40cf607af0920c96a67a5ee0e32e5db90e2192d1
soblab <soblab@Whit209-15.it.farmingdale.edu>
2023-10-05 13:21:02 -0400

    change the addEven window

--- src/main/java/com/mycompany/personalstudyingschedulingapplication/AddEventController.java
+++ src/main/java/com/mycompany/personalstudyingschedulingapplication/AddEventController.java
@@ -4,55 +4,82 @@
  */
 package com.mycompany.personalstudyingschedulingapplication;
 
-
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class AddEventController {
 
     @FXML
-    private TextField startTimeField;
-
-    @FXML
-    private RadioButton MONButton;
-
-    @FXML
-    private RadioButton THUButton;
-
-    @FXML
-    private MenuButton ColorButton;
-
-    @FXML
     private TextField titleField;
+
+    @FXML
+    private TextField startTimeField;
 
     @FXML
     private TextField endTimeField;
 
     @FXML
-    private RadioButton SATButton;
-
-    @FXML
-    private Button createEvenButton;
-
-    @FXML
-    private RadioButton WEDButton;
+    private RadioButton MONButton;
 
     @FXML
     private RadioButton TUEButton;
 
     @FXML
+    private RadioButton WEDButton;
+
+    @FXML
+    private RadioButton THUButton;
+
+    @FXML
     private RadioButton FRIButton;
+
+    @FXML
+    private RadioButton SATButton;
 
     @FXML
     private RadioButton SUNButton;
 
     @FXML
+    private DatePicker startDayField;
+
+    @FXML
+    private ColorPicker ColorButton;
+
+    @FXML
+    private Button createEvenButton;
+
+    @FXML
+    void ColorButtonSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FRIButtonSelect(ActionEvent event) {
+
+    }
+
+    @FXML
     void MONButtonSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SATButtonSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SUNButtonSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void THUButtonSelect(ActionEvent event) {
 
     }
 
@@ -67,34 +94,10 @@ public class AddEventController {
     }
 
     @FXML
-    void THUButtonSelect(ActionEvent event) {
+    void createEvenButtonPress(ActionEvent event) {
 
-    }
-
-    @FXML
-    void FRIButtonSelect(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SATButtonSelect(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SUNButtonSelect(ActionEvent event) {
-        
-    }
-
-    @FXML
-    void ColorButtonSelect(ActionEvent event) {
-        
-    }
-
-    @FXML
-    void createEvenButtonPress(ActionEvent event) throws IOException {
-        App.setRoot("scheduleScreen");
     }
 
 }
+
 
