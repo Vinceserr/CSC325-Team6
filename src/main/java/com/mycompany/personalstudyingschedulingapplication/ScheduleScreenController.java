480d3b0ecaa725b4192300494f01d3aff60066c0
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2023-10-05 23:29:35 -0400

    Modify Add Event and Schedule Screen

--- src/main/java/com/mycompany/personalstudyingschedulingapplication/ScheduleScreenController.java
+++ src/main/java/com/mycompany/personalstudyingschedulingapplication/ScheduleScreenController.java
@@ -2,56 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.personalstudyingschedulingapplication;
 
+package com.mycompany.personalstudyingschedulingapplication;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.GridPane;
-
-/**
- * FXML Controller class
- *
- * @author user
- */
 
 public class ScheduleScreenController {
 
     @FXML
-    private Button generateButton;
+    private Button addEvent;
 
     @FXML
-    private Button ExitButton;
+    private Button clearAllButton;
 
     @FXML
-    private Button SaveButton;
+    private TableColumn<?, ?> eventLoop;
+
+    @FXML
+    private TableColumn<?, ?> eventColor;
+
+    @FXML
+    private TableColumn<?, ?> eventTime;
+
+    @FXML
+    private TableColumn<?, ?> eventTiltle;
 
     @FXML
     private GridPane schedulePane;
 
     @FXML
-    private Button addEventButton;
-
-    @FXML
-    void SaveButtonPress(ActionEvent event) {
-            
-    }
-
-    @FXML
     void addEventButtonPress(ActionEvent event) throws IOException {
-        App.setRoot("addEvent");
+          App.setRoot("addEvent");
     }
 
     @FXML
-    void ExitButtonPress(ActionEvent event) {
-        System.exit(0);
-    }
-
-    @FXML
-    void generateButtonPress(ActionEvent event) {
+    void clearAllButtonPress(ActionEvent event) {
 
     }
 
 }
+
 
