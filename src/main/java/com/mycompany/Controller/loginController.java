16385cfef158a48b12d9cff5f0c019fecdc243d4
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2023-11-15 23:06:54 -0500

    Change the program structure to MVVM create module file and move Account and Task class into. create viewModule file and add AccountViewModel into. add the firebase system from Brian update the Account.java update the signup.fxml update the signupController.java update the loginController.java

--- src/main/java/com/mycompany/Controller/loginController.java
+++ src/main/java/com/mycompany/Controller/loginController.java
@@ -1,9 +1,11 @@
 package com.mycompany.Controller;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import com.mycompany.Application.*;
 import com.mycompany.Model.Account;
+import com.mycompany.ViewModel.AccountViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -11,16 +13,21 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
+import static com.mycompany.Controller.signupController.getAccountDetails;
+
 public class loginController {
 
     @FXML
     private TextField tf_userName;
 
-
     @FXML
     private TextField tf_password;
 
-
+    public void initialize(){
+        AccountViewModel accountViewModel = new AccountViewModel();
+        tf_userName.textProperty().bindBidirectional(accountViewModel.usernameProperty());
+        tf_password.textProperty().bindBidirectional(accountViewModel.passwordProperty());
+    }
     @FXML
     void signInButtonPress(ActionEvent event) throws Exception {
         ArrayList<Account>list = signupController.list;
@@ -42,18 +49,22 @@ public class loginController {
 
 
     }
-    boolean login(ArrayList<Account> list){
-        if(!list.isEmpty()){
-            for(Account a:list){
-                String username = a.getUsername();
-                String password =a.getPassword();
-                String usernameText = tf_userName.getText();
-                String passwordText = tf_password.getText();
+    boolean login(ArrayList<Account> list) throws ExecutionException, InterruptedException {
+        String userNameText = tf_userName.getText();
+        String passwordText = tf_password.getText();
+        Account account = getAccountDetails(userNameText);
+        //if nothing enter, click logIn button will not work
+        if (userNameText.isEmpty() || passwordText.isEmpty()) {
+            return false;
+        }
+        //if there is an account created, enter the correct credentials
+        if (account != null) {
+            String username = account.getUsername();
+            String password = account.getPassword();
 
-                if(username.equals(usernameText) &&
-                        password.equals(passwordText)){
-                    return true;
-                }
+            if (username.equals(userNameText) &&
+                    password.equals(passwordText)) {
+                return true;
             }
         }
         return false;
--- src/main/java/com/mycompany/Controller/signupController.java
+++ src/main/java/com/mycompany/Controller/signupController.java
@@ -1,7 +1,15 @@
 package com.mycompany.Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.mycompany.Application.App;
 import com.mycompany.Model.Account;
 import com.mycompany.Application.CreateStage;
 import javafx.event.ActionEvent;
@@ -13,23 +21,19 @@ import javafx.scene.control.TextField;
 
 public class signupController {
 
-    @FXML
-    private TextField usernameField;
+    @FXML private TextField usernameField;
+    @FXML private TextField emailField;
 
-    @FXML
-    private Button submitButton;
+    @FXML private TextField passwordField;
+    @FXML private TextField newPasswordField;
 
-    @FXML
-    private TextField newPasswordField;
-
-    @FXML
-    private TextField passwordField;
+    @FXML private Button submitButton;
 
     static ArrayList<Account> list = new ArrayList<>();
 
     @FXML
     void submitButtonPress(ActionEvent event) throws Exception {
-        boolean result = register(list);
+        boolean result = register();
         //if is true, go back to signIn menu
         if(result){
             CreateStage.close();
@@ -44,46 +48,72 @@ public class signupController {
         }
 
     }
-    boolean register(ArrayList<Account> list){
+    /**
+     * getAccountDetails method:
+     * This method get the specific user from the database by using the email.
+     *
+     * @param username
+     * @return the Account info
+     */
+    public static Account getAccountDetails(String username) throws ExecutionException, InterruptedException {
+        //document reference for the specific email(document name in the database)
+        DocumentReference docRef = App.fstore.collection("Users").document(username);
+        //we need the document snapShot retrieved from the document reference
+        ApiFuture<DocumentSnapshot> ft = docRef.get();
+        //extract the document snapShot from the APiFuture object
+        DocumentSnapshot doc = ft.get();
+        //if the document exists return the Account object
+        if (doc.exists()) {
+            Account ac = new Account(String.valueOf(doc.getData().get("email")),
+                    String.valueOf(doc.getData().get("name").toString()),
+                    String.valueOf(doc.getData().get("password").toString()));
+            return ac;
+        } else {
+            return null;
+        }
+    }
+
+    boolean register() throws ExecutionException, InterruptedException{
 
         String usernameTF  = usernameField.getText();
+        String emailTF = emailField.getText();
         String passwordTF = passwordField.getText();
         String newpasswordTF = newPasswordField.getText();
         Account newAccount = new Account();
 
         //if nothing enter, click submit button will not work
-        if(usernameTF.isEmpty() || passwordTF.isEmpty()){
+        if(emailTF.isEmpty() ||usernameTF.isEmpty() || passwordTF.isEmpty()){
             return false;
         }
         // if it is empty, only need to check the password
         if(list.isEmpty()){
             if(passwordTF.equals(newpasswordTF)){
-
                 newAccount.setUsername(usernameTF);
+                newAccount.setEmail(emailTF);
                 newAccount.setPassword(passwordTF);
                 list.add(newAccount);
-
                 return true;
             }
         }
 
-        //check this account is not exist
-        for(Account a:list){
-            String username = a.getUsername();
-            String password =a.getPassword();
-
-            // check username is not exit and two passsword enters are correct
-            if(!username.equals(usernameTF) &&
-                    passwordTF.equals(newpasswordTF)){
-
-                newAccount.setUsername(usernameTF);
-                newAccount.setPassword(passwordTF);
-                list.add(newAccount);
-
-                return true;
-            }
+        //check if this account exist by calling the getAccountDetails
+        newAccount = getAccountDetails(usernameTF);
+        //if user is not found then add the new User
+        if (newAccount == null) {
+            //add User to the database
+            DocumentReference docRef = App.fstore.collection("Users").document(usernameTF);
+            //Add document data using a hashMap
+            Map<String, Object> data = new HashMap<>();
+            data.put("email", emailTF);
+            data.put("name", usernameTF);
+            data.put("password", passwordTF);
+            ApiFuture<WriteResult> result = docRef.set(data);
+            //print statement to see if user was added to the database
+            System.out.println("UserAdded");
+            return true;
         }
-
+        //if user already registered then he/she cant register again
+        //and return false
         return false;
 
     }
