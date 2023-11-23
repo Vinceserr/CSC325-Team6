0f7beaddf3b9c1ce71a377bcb90b179211b94ba2
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2023-11-22 22:05:33 -0500

    Added UserPrefs classes

---
+++ src/main/java/com/mycompany/Application/UserPrefsStage.java
@@ -0,0 +1,14 @@
+package com.mycompany.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+public class UserPrefsStage extends createStage{
+    @Override
+    public void showStage()throws Exception {
+        loader = new FXMLLoader(getClass().getResource("/com/mycompany/Application/UserPrefs.fxml"));
+        scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
