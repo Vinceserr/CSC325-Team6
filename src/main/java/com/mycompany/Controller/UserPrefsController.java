d5c36f9ec7429f3f31cf337c7b6f5f00bb16a28f
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2023-12-10 16:04:56 -0500

    update the userPer

--- src/main/java/com/mycompany/Controller/UserPrefsController.java
+++ src/main/java/com/mycompany/Controller/UserPrefsController.java
@@ -2,6 +2,7 @@ package com.mycompany.Controller;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,8 +18,9 @@ import java.time.temporal.ChronoUnit;
 
 public class UserPrefsController {
 
-    @FXML private ComboBox<String> genEndHours;
-    @FXML private ComboBox<String> genEndMinutes;
+    @FXML private ComboBox<String> durationHours;
+    @FXML private ComboBox<String> durationMinutes;
+    @FXML private ComboBox<String> durationSeconds;
 
     @FXML private Label alarmTime;
 
@@ -26,14 +28,16 @@ public class UserPrefsController {
     private Timeline clock;
 
     public void initialize(){
+        durationHours.setValue("00");
+        durationMinutes.setValue("00");
+        durationSeconds.setValue("00");
         setTimeOfComboBox();
-        setTimeToNow();
-
     }
 
     @FXML
     void onStartPress() {
-        clock = new Timeline(new KeyFrame(Duration.ZERO, e -> updateLabel()),
+        setAlarm();
+        clock = new Timeline(new KeyFrame(Duration.ZERO, e -> updateAlarmTimeLabel()),
                 new KeyFrame(Duration.seconds(1)));
         clock.setCycleCount(Timeline.INDEFINITE);
         clock.play();
@@ -43,83 +47,18 @@ public class UserPrefsController {
     void onStopPress() {
         clock.stop();
     }
-    private void setTimeToNow() {
-        LocalDateTime now = LocalDateTime.now();
-
-        genEndHours.setValue(String.format("%02d", now.getHour()));
-        genEndMinutes.setValue(String.format("%02d", now.getMinute()));
-    }
-
-    private void updateLabel() {
-        updateAlarmTimeLabel();
-        setAlarm();
-    }
-
-    private void setAlarm() {
-        try {
-            LocalDate today = LocalDate.now();
-            if (genEndHours.getValue() != null && genEndMinutes.getValue() != null) {
-                LocalTime newEndTime = LocalTime.of(Integer.parseInt(genEndHours.getValue()), Integer.parseInt(genEndMinutes.getValue()));
-                LocalDateTime newGenEndTime = LocalDateTime.of(today, newEndTime);
-
-                // only update when genEndTime is change
-                if (newGenEndTime.equals(genEndTime)) {
-                    return;
-                }
-
-                genEndTime = newGenEndTime;
-                updateAlarmTimeLabel(); // update alarmTime
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid number format in ComboBox");
-        }
-    }
-
-    private void checkAlarm() {
-        if (alarmTime.getText().equals("00:00:00")) {
-            clock.stop();
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Time's Up!");
-            alert.setHeaderText(null);
-            alert.setContentText("The time has been reached!");
-
-            alert.show();
-        }
-    }
-    private void updateAlarmTimeLabel() {
-        if (genEndTime == null) {
-            return;
-        }
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime endDateTime = LocalDateTime.of(now.toLocalDate(), LocalTime.from(genEndTime));
-
-        // check current time is after than genEndTime
-        if (now.toLocalTime().isAfter(genEndTime.toLocalTime())) {
-            // if current is after genEndTime，than set genEndTime is next day
-            endDateTime = LocalDateTime.of(now.toLocalDate().plusDays(1), LocalTime.from(genEndTime));
-        } else {
-            endDateTime = LocalDateTime.of(now.toLocalDate(), LocalTime.from(genEndTime));
-        }
-        if (now.isBefore(endDateTime)) {
-            long hours = now.until(genEndTime, ChronoUnit.HOURS);
-            long minutes = now.until(genEndTime, ChronoUnit.MINUTES) % 60;
-            long seconds = now.until(genEndTime, ChronoUnit.SECONDS) % 60;
-            alarmTime.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
-        } else {
-            alarmTime.setText("00:00:00");
-
-        }
-        checkAlarm();
-    }
 
     private void setTimeOfComboBox(){
         ObservableList<String> hours = generateTimeList(24);
         ObservableList<String> minutes = generateTimeList(60);
 
-        genEndHours.setItems(hours);
-        genEndMinutes.setItems(minutes);
+        durationHours.setItems(hours);
+        durationMinutes.setItems(minutes);
+        durationSeconds.setItems(minutes);
+
+
     }
+
     // used to set the hours and minutes
     private ObservableList<String> generateTimeList(int max) {
         ObservableList<String> timeList = FXCollections.observableArrayList();
@@ -129,4 +68,49 @@ public class UserPrefsController {
         return timeList;
     }
 
+
+    // set the ending time
+    private void setAlarm() {
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            int selectedHours = Integer.parseInt(durationHours.getValue());
+            int selectedMinutes = Integer.parseInt(durationMinutes.getValue());
+            int selectedSeconds = Integer.parseInt(durationSeconds.getValue());
+
+            genEndTime = now.plusHours(selectedHours).plusMinutes(selectedMinutes).plusSeconds(selectedSeconds);
+
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format in ComboBox");
+        }
+    }
+
+    private void checkAlarm() {
+        if (alarmTime.getText().equals("00:00:00")) {
+            clock.stop();
+
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Time's Up!");
+                alert.setHeaderText(null);
+                alert.setContentText("The time has been reached!");
+
+                alert.showAndWait();
+            });
+        }
+    }
+    private void updateAlarmTimeLabel() {
+        System.out.println(genEndTime);
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isBefore(genEndTime)) {
+            long hours = now.until(genEndTime, ChronoUnit.HOURS);
+            long minutes = now.until(genEndTime, ChronoUnit.MINUTES) % 60;
+            long seconds = now.until(genEndTime, ChronoUnit.SECONDS) % 60;
+            alarmTime.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+        } else {
+            alarmTime.setText("00:00:00");
+            checkAlarm();
+        }
+
+    }
 }
